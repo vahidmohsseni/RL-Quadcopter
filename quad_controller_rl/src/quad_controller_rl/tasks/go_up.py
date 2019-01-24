ec535276804f98504592fdd80f9fc4ff5dc601b9
vahidmohsseni <vahidmohsseni@yahoo.com>
2019-01-25 02:52:35 +0330

    ...

--- quad_controller_rl/src/quad_controller_rl/tasks/go_up.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/go_up.py
@@ -8,4 +8,10 @@ class GoUp(BaseTask):
     def __init__(self):
         self.max_duration = 5
         print("****1")
+        cube_size = 300.0  # env is cube_size x cube_size x cube_size
+        self.observation_space = spaces.Box(
+            np.array([- cube_size / 2, - cube_size / 2,       0.0, -1.0, -1.0, -1.0, -1.0]),
+            np.array([  cube_size / 2,   cube_size / 2, cube_size,  1.0,  1.0,  1.0,  1.0]))
+        print("Takeoff(): observation_space = {}".format(self.observation_space))  # [debug]
+
 
